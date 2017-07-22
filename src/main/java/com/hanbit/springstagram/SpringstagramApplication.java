1157ae75c6bee113e3720c7256e9532cf58f9b77
hb4000 <hb4000@hb4000-PC>
2017-07-22 13:20:10 +0900

    initial commit

---
+++ src/main/java/com/hanbit/springstagram/SpringstagramApplication.java
@@ -0,0 +1,12 @@
+package com.hanbit.springstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstagramApplication.class, args);
+	}
+}
